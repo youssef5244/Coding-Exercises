9a627ea34d09f93c5871f7b5b6a9603e8a122382
youssef5244 <youssefmatar1990@gmail.com>
2024-08-04 00:54:45 +0300

    Create Solution.jsx

---
+++ exercise16/Solution.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+
+// don't change the Component name "App"
+function App() {
+    
+const[ffeedback, setFeedback]=React.useState('');
+const[name, setName]=React.useState('');
+
+function handleChange(event){
+    setFeedback(event.target.value);
+}
+
+function handleChange2(event){
+    setName(event.target.value);
+}
+
+  return (
+    <>
+      <section id="feedback">
+        <h2>Please share some feedback</h2>
+        <p>
+          <label>Your Feedback</label>
+          <textarea value={ffeedback} onChange={handleChange} />
+        </p>
+        <p>
+          <label>Your Name</label>
+          <input type="text" value={name} onChange={handleChange2} />
+        </p>
+      </section>
+      <section id="draft">
+        <h2>Your feedback</h2>
+
+        <Review feedback={ffeedback} student={name}/>
+
+        <p>
+          <button>Save</button>
+        </p>
+      </section>
+    </>
+  );
+}
+
+export default App;
